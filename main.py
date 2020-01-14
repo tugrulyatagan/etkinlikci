51a9d731cc9168121d013db209cacd1bcbb25259
Tugrul Yatagan <tugrulyatagan@gmail.com>
2020-01-14 23:06:36 +0300

    json parser and email sender

--- main.py
+++ main.py
@@ -2,26 +2,57 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 import json
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_email(subject, body):
+    mailconf = open("mail.txt", "r")
+    from_user = mailconf.readline()
+    from_password = mailconf.readline()
+    to = mailconf.readline()
+
+    message = MIMEText(body, 'plain', 'utf-8')
+    message['Subject'] = subject
+    message['From'] = from_user
+    message['To'] = to
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(from_user, from_password)
+    server.sendmail(from_user, to, message.as_string())
+    server.close()
+    print("email is sent")
 
 
 def get_tiyatrolar_sessions(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     sessions = soup.find(id="activity_session").find_all('p')
+    result = []
     for session in sessions:
         if "BİLET AL" in session.text:
             datestr = session.contents[1]
             datesplit = datestr.split()
             date = datetime.datetime.strptime("{} {}".format(datesplit[0], datesplit[3]), '%d.%m.%Y %H:%M')
             placestr = session.contents[3].text
-            print("{}  {}".format(date, placestr))
+            datestr = date.strftime("%Y-%m-%d-%H-%M")
+            result.append({"date" : datestr, "place" : placestr})
+    return result
 
 
-url = "https://tiyatrolar.com.tr/tiyatro/bir-baba-hamlet"
-url = "https://tiyatrolar.com.tr/tiyatro/cimri5"
-#get_tiyatrolar_sessions(url)
+with open('events.json', 'r', encoding='utf-8') as json_file:
+    events = json.load(json_file)
 
+for event in events:
+    tiyatrolar_sessions = get_tiyatrolar_sessions(event["url"])
+    for tiyatrolar_session in tiyatrolar_sessions:
+        if tiyatrolar_session not in event["sessions"]:
+            print("New session is found for {}".format(event["url"]))
+            email_body="New session is found for:\nURL: {}\nDate: {}\nPlace: {}\n".format(event["url"], tiyatrolar_session["date"], tiyatrolar_session["place"])
+            send_email("New session is found", email_body)
+            print(tiyatrolar_session)
+            event["sessions"].append({"date" : tiyatrolar_session["date"], "place" : tiyatrolar_session["place"]})
 
-with open('etkinlikler.json') as json_file:
-    data = json.load(json_file)
-    print(data)
+with open('events.json', 'w', encoding='utf-8') as json_file:
+    json.dump(events, json_file, ensure_ascii=False, indent=4)
